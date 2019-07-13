879ab17778bdec6f6fca4bf28edd53dba884b24a
Sahil Ajmera <32546795+the-true-beginner@users.noreply.github.com>
2019-07-13 19:56:37 -0400

    Refactor code and add functions for date, time and day and also for getting information from Wikipedia

--- jarvis.py
+++ jarvis.py
@@ -5,6 +5,8 @@ import webbrowser
 import os
 import random
 import sys
+import datetime
+import wikipedia
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -50,6 +52,95 @@ def wishMe():
 	else:
 		speak("Good Evening sir. Jarvis at your service")
 
+def open_youtube():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('youtube.com')
+
+def open_google():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('google.com')	
+
+def show_mycourses():
+	path = "D:\\"
+	os.startfile(path)
+
+def open_mycourses():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('mycourses.rit.edu')
+
+def open_linkedin():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('linkedin.com')
+
+def open_sis():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('sis.rit.edu')
+
+def open_github():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('github.com')
+
+def open_tigercenter():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('tigercenter.rit.edu')
+
+def open_gmail():
+	chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+	webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
+	webbrowser.get('chrome').open_new('gmail.com')
+
+def play_music_offline():
+	music_dir = 'D:\\songs'
+	songs = os.listdir(music_dir)
+	songnumber = random.randint(0,len(songs))
+	os.startfile(os.path.join(music_dir,songs[songnumber]))
+
+def open_sublime_text():
+	path = "D:\\Program Files\\Sublime Text 3\\sublime_text.exe"
+	os.startfile(path)
+
+def open_intelliJ():
+	path = "D:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2017.3\\bin\\idea64.exe"
+	os.startfile(path)
+
+def open_pycharm():
+	path = "D:\\Program Files\\JetBrains\\PyCharm Community Edition 2019.1\\bin\\pycharm64.exe"
+	os.startfile(path)
+
+def open_cmd():
+	os.system("start cmd.exe")
+
+def current_day():
+	dict = {0:"Monday",1:"Tuesday",2:"Wednesday",3:"Thursday",4:"Friday",5:"Saturday",6:"Sunday"}
+	speak("It is "+dict[datetime.datetime.today().weekday()])
+
+def current_date():
+	strtime = datetime.datetime.now().strftime("%d-%B-%Y")
+	speak(f"Sir , Today's date is {strtime}")
+
+def current_time():
+	strtime = datetime.datetime.now().strftime("%H:%M:%S")
+	speak(f"Sir , the current time is {strtime}")
+
+def search_wikipedia(query):
+	results = wikipedia.summary(query, sentences=2)
+	speak("According to Wikipedia")
+	speak(results)
+
+def mute():
+	speak('From now on, I will let you focus sir.')
+	listen = False
+
+def shutdown():
+	sys.exit()
+
 def main():
 	wishMe()
 	listen = True
@@ -83,61 +174,46 @@ def main():
 		if listen != False:
 
 			if 'open youtube' in query or 'open youtube.com' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('youtube.com')
+				open_youtube()
 			elif 'open google' in query or 'open google.com' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('google.com')
+				open_google()
 			elif 'show my courses' in query:
-				path = "D:\\"
-				os.startfile(path)
+				show_mycourses()
 			elif 'open my courses' in query or 'mycourses' in query or 'my courses' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('mycourses.rit.edu')
+				open_mycourses()
 			elif 'open linkedin' in query or 'linkedin' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('linkedin.com')
+				open_linkedin()
 			elif 'open sis' in query or 'sis' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('sis.rit.edu')
+				open_sis()
 			elif 'open github' in query or 'github' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('github.com')
+				open_github()
 			elif 'open tigercenter' in query or 'tigercenter' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('tigercenter.rit.edu')
+				open_tigercenter()
 			elif 'show me my emails' in query or 'gmail' in query:
-				chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-				webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
-				webbrowser.get('chrome').open_new('gmail.com')
+				open_gmail()
 			elif 'music' in query or 'song' in query or 'play anything' in query:
-				music_dir = 'D:\\songs'
-				songs = os.listdir(music_dir)
-				songnumber = random.randint(0,len(songs))
-				os.startfile(os.path.join(music_dir,songs[songnumber]))
+				play_music_offline()
 			elif 'open sublime text' in query or 'open sublime' in query:
-				path = "D:\\Program Files\\Sublime Text 3\\sublime_text.exe"
-				os.startfile(path)
+				open_sublime_text()
 			elif 'open intellij' in query or 'intellij' in query:
-				path = "D:\\Program Files\\JetBrains\\IntelliJ IDEA Community Edition 2017.3\\bin\\idea64.exe"
-				os.startfile(path)
+				open_intelliJ()
 			elif 'open pycharm' in query or 'pycharm' in query:
-				path = "D:\\Program Files\\JetBrains\\PyCharm Community Edition 2019.1\\bin\\pycharm64.exe"
-				os.startfile(path)
+				open_pycharm()
 			elif 'open cmd' in query or 'cmd' in query:
-				os.system("start cmd.exe")
-			elif 'shut up' in query or 'take a break' in query:
-				speak('From now on, I will let you focus sir.')
-				listen = False
+				open_cmd()
+			elif 'shut up' in query or 'take a break' in query or 'mute yourself' in query:
+				mute()
 			elif 'shut down' in query or 'exit' in query:
-				sys.exit()
+				shutdown()
+			elif 'time' in query:
+				current_time()
+			elif 'date' in query:
+				current_date()
+			elif 'day' in query:
+				current_day()
+			elif 'wikipedia' in query or 'who is' in query or 'what is' in query:
+				query = query.replace("wikipedia", "")
+				search_wikipedia(query)
 			else:
 				speak('Sorry sir. Can you repeat that?')
 
