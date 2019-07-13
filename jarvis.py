a38fb620ca23cf965284e56f7c0b4d4a52533a04
Sahil Ajmera <32546795+the-true-beginner@users.noreply.github.com>
2019-07-13 08:55:40 -0400

    Fix existing bugs( check of in string for each or in if condition) and add code for opening sublime text editor

--- jarvis.py
+++ jarvis.py
@@ -21,7 +21,7 @@ def takeCommand():
 	r = sr.Recognizer()
 	with sr.Microphone() as source:
 		print("Listening ...")
-		r.pause_threshold = 1.5
+		r.pause_threshold = 2
 		audio = r.listen(source)
 
 	try:
@@ -68,20 +68,23 @@ def main():
 		# Open Google
 		# Search results for google 
 		'''
-		if 'open youtube' in query:
+		if 'open youtube' in query or 'open youtube.com' in query:
 			chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
 			webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
 			webbrowser.get('chrome').open_new('youtube.com')
-		if 'open google' in query:
+		elif 'open google' in query or 'open google.com' in query:
 			chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
 			webbrowser.register('chrome', None,webbrowser.BackgroundBrowser(chrome_path),1)
 			webbrowser.get('chrome').open_new('google.com')
-		if 'music' or 'song' or 'play anything' in query:
-			music_dir = 'C:\\Users\\Sahil Ajmera\\Music'
+		elif 'music' in query or 'song' in query or 'play anything' in query:
+			music_dir = 'D:\\songs'
 			songs = os.listdir(music_dir)
 			songnumber = random.randint(0,len(songs))
 			os.startfile(os.path.join(music_dir,songs[songnumber]))
-		
+		elif 'open sublime text' in query or 'open sublime' in query:
+			path = "D:\\Program Files\\Sublime Text 3\\sublime_text.exe"
+			os.startfile(path)
+
 
 
 
